1431235e54f5c3751d971420b175088aae72566c
saadullah dar <saadullahdar.official@gmail.com>
2025-01-23 12:00:23 +0500

    variables in javaScript

---
+++ chapter 3/variables.js
@@ -0,0 +1,14 @@
+var legacy = 1      // using legacy var key word
+let number = 1;     // using number literal
+let string = 'Hello.'   // using string literal
+let array = []          // using array literal
+let object = {}         // using object literal
+let json = {"a" : 1}    // assign json (object)
+let json2 = '{"a": 1}'  // assign json2 (string)
+let boolean = true      // assign boolean literal
+let inf = Infinity      // assign Infinity (number)
+let func = function (a) {return a}  // assign function expression 
+let arrow = (a) => { a } // assign arrow function
+let fp = a => a         // assign arrow expression
+let n = new Number(1)    // assign number object
+let o = new Object()    // assign blank object
