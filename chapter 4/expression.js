bfbd996aef250d359b934520a5e586812db4574c
saadullah dar <saadullahdar.official@gmail.com>
2025-01-23 15:14:05 +0500

    expression in JavaScript

---
+++ chapter 4/expression.js
@@ -0,0 +1,6 @@
+// a statement that produces a single value other than undefined can be referred to as an expression
+
+console.log(1 + 1) // expression
+
+let f = function() {return 1}   // statement
+f() // 1 -> function expression
