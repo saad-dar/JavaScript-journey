a09bd94a8359dd21380f771412f3ddf976085b5e
saadullah dar <saadullahdar.official@gmail.com>
2025-02-03 01:18:08 +0500

    parentices on literal values in javaScript

---
+++ chapter 5/parentices-on-literal-values.js
@@ -0,0 +1,9 @@
+console.log(5 * 10 + 2 === 5 * (10 + 2))
+
+// 1.toString(); // Error : Invalid or unexpected token
+
+console.log((1).toString());
+
+console.log("hello".toUpperCase());
+
+console.log(new Number(1).toString())
