a380b3260ac1f5b0ed0c2c5e3cf70c654e59c7df
saadullah dar <saadullahdar.official@gmail.com>
2025-02-04 14:16:57 +0500

    string and number comparison in javascript

---
+++ type-coersion-madness/string-to-number-comparison.js
@@ -0,0 +1,10 @@
+// comparing numeric string to numbers
+
+console.log(1 == '1'); // true
+
+console.log('1' == 1); // true
+
+// comparing non-numeric strings to numbers
+
+console.log(1 == 'a'); // 1 == NaN
+console.log('a' == 1); // NaN == 1
