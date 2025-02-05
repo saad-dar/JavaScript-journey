36f2c7d13c0655523cc36cc14045598730ede509
saadullah dar <saadullahdar.official@gmail.com>
2025-02-06 03:01:18 +0500

    variable declaration using const

---
+++ chapter 7/const.js
@@ -0,0 +1,8 @@
+let a;
+console.log(a); // undefined
+
+// const b; // SyntaxError: Missing initializer in const declaration
+
+const speed_of_light = 186000; // Miles per second
+// speed_of_light = 1; // TypeError: Assignment to constant variable.
+
