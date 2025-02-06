f18fdc0f8be7756c4ed532b1f44d20b9df20d9b0
saadullah dar <saadullahdar.official@gmail.com>
2025-02-06 11:39:31 +0500

    arithmetic operators in javascript

---
+++ chapter8/arithmetic.js
@@ -0,0 +1,12 @@
+let variable = 1;
+
+console.log(variable + 2);
+console.log(variable - 1);
+console.log(variable / 2);
+console.log(variable * 5);
+console.log(variable * variable);
+
+console.log(variable++);
+console.log(variable++);
+console.log(variable++);
+console.log(variable--);
