8b1dc7fc03c7084bc44a567d12d3f546e8674bdb
saadullah dar <saadullahdar.official@gmail.com>
2025-02-06 11:50:01 +0500

    comparison in javascript

---
+++ chapter8/comparison.js
@@ -0,0 +1,23 @@
+// equality
+console.log(1 == 1) // true
+console.log('1' == 1) // true
+console.log(1 == 2) // false
+
+// equality of value and type
+console.log('1' === '1') // true
+console.log(1 === 1) // true
+console.log( 1 === '1' )  // false
+
+// inequality
+console.log( 1 != 1) // false
+console.log(1 != 2) // true
+
+// inequality of value and type
+console.log('1' !== 1) // true
+console.log(1 !== 1) // false
+
+// comparison (less than and greater than)
+console.log(2 > 1) // true
+console.log(5 < 7) // true
+console.log(2 >= 1) // true
+console.log(2 <= 1) // false
