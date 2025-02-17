bd6f6f6094b8c7de66d975628dcd49d2c9590d34
saadullah dar <saadullahdar.official@gmail.com>
2025-02-17 08:17:46 +0500

    typeof operator in javascript

---
+++ chapter8/typeof.js
@@ -0,0 +1,17 @@
+console.log(typeof 1);
+console.log(typeof Number(1));
+console.log(typeof new Number(1));
+
+
+// the value produced by the typeof operator is always string format
+
+console.log(typeof 125);
+console.log(typeof 100n);
+console.log(typeof 'text');
+console.log(typeof NaN);
+console.log(typeof true);
+console.log(typeof []);
+console.log(typeof {});
+console.log(typeof Object);
+console.log(typeof new Object());
+console.log(typeof null);
