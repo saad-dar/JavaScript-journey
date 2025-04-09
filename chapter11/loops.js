615320b323dbde8b0c4c2415032bb2e2b943ad7b
saadullah dar <saadullahdar.official@gmail.com>
2025-04-09 16:38:37 +0500

    increamenting using for loop

---
+++ chapter11/loops.js
@@ -0,0 +1,22 @@
+// You can say that an iterable object abstracts away the index values of
+//  a list and helps you focus on solving the problem
+
+// Array is an iterable. Object is not (objects are enumerable).
+
+// An iterable type guarantees the order of items in the set. This is why arrays have
+//  an index for each item. Enumerable types do not guarantee the order in which
+//  properties will appear when iterated
+
+// increamenting and reducing 
+
+let miles = [5, 12, 75, 2, 5];
+
+
+let A = 0;
+
+for(let i = 0; i < 5; i++) {
+    A += miles[i];
+}
+
+console.log(A); // 99
+
