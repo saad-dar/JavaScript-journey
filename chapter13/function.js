6cf9dad3bcc0c405a824d955fdc1ada365d3600b
saadullah dar <saadullahdar.official@gmail.com>
2025-04-16 18:39:03 +0500

    passing a function another function parameter in javaScript

--- chapter13/function.js
+++ chapter13/function.js
@@ -6,8 +6,24 @@ function update (a, b, c, d = "hi") {
 
 update(7, [], {}, "hi")
 
-setTimeout(function() {
-    console.log("print something in 1 secnod.");
-    console.log(arguments)
-}, 1000);
+// setTimeout(function() {
+//     console.log("print something in 1 secnod.");
+//     console.log(arguments)
+// }, 1000);
 // console.log("after set time out function.")
+
+function Fun(text, number, array, object, func, misc) {
+
+    // output the argument values
+    console.log(text);
+    console.log(number);
+    console.log(array);
+    console.log(object);
+    console.log(misc);
+
+    console.log(func());
+}
+
+function Volleyball() { return "Volleyball" }
+
+Fun("Text", 125, [1,2,3], {count: 1}, Volleyball, Volleyball());
