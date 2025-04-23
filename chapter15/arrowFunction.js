b6716a21a918f0ccbeda2939aa3f856ea1cac4ee
saadullah dar <saadullahdar.official@gmail.com>
2025-04-23 14:54:02 +0500

    arrow function syntax with arguments

--- chapter15/arrowFunction.js
+++ chapter15/arrowFunction.js
@@ -21,3 +21,22 @@ console.log(fun_3());
 let expression = e => e;
 
 console.log(expression(1)) // 1
+
+// arguments
+
+// arrow function syntax with arguments
+
+let x  = (arg1, arg2) => { console.log(arg1, arg2); };
+
+x(1, 3); // 1 3
+
+let boomerang = a => "returns";
+
+let karma = a => { return "returns"; }
+
+let prayer = a => { return Math.random() >= 0.5 }
+let time = a => { "won't return"; }
+
+boomerang(1); // "returns"
+karma(1); // "returns"
+time(1); // [undefinec]
