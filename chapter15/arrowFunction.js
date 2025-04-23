4f91a8c74dc92e26856b36608b3d75046db6f79c
saadullah dar <saadullahdar.official@gmail.com>
2025-04-23 14:53:15 +0500

    return a value from an arrow function

--- chapter15/arrowFunction.js
+++ chapter15/arrowFunction.js
@@ -7,3 +7,17 @@ let fun_1 = () => {};
 
 // call arrow function by its name
 fun_1();
+
+// return a value from an arrow function
+
+let fun_2 = () => { return 1; }
+
+console.log(fun_2()); // 1
+
+let fun_3 = () => 1
+
+console.log(fun_3());
+
+let expression = e => e;
+
+console.log(expression(1)) // 1
