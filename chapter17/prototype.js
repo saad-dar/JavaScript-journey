7a90a161ac4ab1082a27fc25683df654f1f53671
saadullah dar <saadullahdar.official@gmail.com>
2025-05-14 18:28:08 +0500

    creating objects using class

--- chapter17/prototype.js
+++ chapter17/prototype.js
@@ -63,3 +63,57 @@ cat.wander = function() {
 }
 
 cat.sleep();
+
+// function as a class in javascript
+
+function Cat(name, hunger, energy, state) {
+    let cat = {};
+
+    cat.name = name;
+    cat.hunger = hunger;
+    cat.energy = energy;
+    cat.state = state;
+
+    cat.sleep = function (amount) {
+        this.state = "sleeping";
+        console.log(`${this.name} is ${this.state}.`);
+        this.energy += 1;
+        this.hunger += 1;
+    }
+
+    cat.wakeup = function(amount) {
+        this.state = "idle";
+        console.log(`${this.name} woke up`);
+    }
+
+    cat.eat = function(amount) {
+        this.state = "eating";
+        console.log(`${this.name} is ${this.state} ${amount} unit(s) of food`);
+        if(this.hunger -= amount <= 0)
+            this.energy += amount;
+        else
+            this.wakeup();
+    }
+
+    cat.wander = function() {
+        this.state = "wandering";
+        console.log(`${this.name} is ${this.state}.`);
+        if(--this.energy < 1) {
+            this.sleep(5)
+        } 
+    }
+
+    return cat;
+}
+
+
+let felix = Cat("Felix", 10, 5, "idle");
+felix.sleep(); // "Felix is sleeping."
+felix.eat(5); // "Felix is eating 5 unit(s) of food."
+felix.wander(); // "Felix is wandering."
+
+let luna = Cat("Luna", 8, 3, "idle");
+luna.sleep();  // "Luna is sleeping."
+luna.wander(); // "Luna is wandering."
+luna.eat(1); // "Luna is eating 1 unit(s) of food."
+
