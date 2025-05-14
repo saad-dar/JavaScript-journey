2760908b9a93501ca6c66f4387861289366aa10f
saadullah dar <saadullahdar.official@gmail.com>
2025-05-14 18:02:07 +0500

    adding functionaly to object

--- chapter17/prototype.js
+++ chapter17/prototype.js
@@ -20,3 +20,46 @@ console.log(literal.__proto__)
 console.log(literal.__proto__.constructor)
 console.log(literal.constructor)
 
+let cat = {};
+
+cat.name = "Felix";
+cat.hunger = 0;
+cat.energy = 1;
+cat.state = "idle";
+
+// sleep to restore the energy
+cat.sleep = function(amount) {
+    this.state = "sleeping";
+    console.log(`${this.name} is ${this.state}.`);
+    this.energy += 1;
+    this.hunger += 1;
+}
+
+// wake up
+cat.wakeup = function() {
+    this.state = "idle";
+    console.log(`${this.name} woke up`);
+}
+
+// eat until hunger is quenched
+cat.eat = function(amount) {
+    this.state = "eating";
+    console.log(`${this.name} is ${this.state} ${amount} unit(s) of food`);
+    if(this.hunger -= amount <= 0)
+        this.energy += amount;
+    else
+        this.wakeup();
+}
+
+// wandaring depletes energy,
+// if necessary, sleep for 5 hours to restore energy
+
+cat.wander = function() {
+    this.state = "wandering";
+    console.log(`${this.name} is ${this.state}.`);
+    if(--this.energy < 1) {
+        this.sleep(5)
+    } 
+}
+
+cat.sleep();
