65d457e8cc37597039249389abc891b3f79ebe36
saadullah dar <saadullahdar.official@gmail.com>
2025-05-14 17:43:45 +0500

    __proto__ object on an object

--- chapter17/prototype.js
+++ chapter17/prototype.js
@@ -4,3 +4,19 @@ console.log(typeof Human.prototype); // object
 
 console.log(Human.prototype.constructor); // false
 console.log(Human.prototype.constructor === Human); // true
+
+// you can also access the prototype of a function using the __proto__ property
+
+let human = new Human("saad");
+
+console.log(human);
+
+let literal = {
+    prop: 123,
+    meth: function() {}
+}
+
+console.log(literal.__proto__)
+console.log(literal.__proto__.constructor)
+console.log(literal.constructor)
+
